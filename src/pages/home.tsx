984b6ee590254f2c25f05da24d54a0f3b0fc323b
Shinemi <simon.lucas.83136@gmail.com>
2026-07-27 12:08:54 +0200

    re added index html due to a mistake + remanaged home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,28 +3,18 @@ import Footer from "../components/footer"
 
 const Home = () => {
     return (
-        <>
-            <html lang="en">
-            <head>
-                <title>Kasa - CP4</title>
-                <link rel="icon" href="../assets/favicon.png" />
-            </head>
-            <body>
-                
-                <header>
-                    <a href="/">Kasa</a>
-                    <Navbar/>
-                </header>
+        <>                
+            <header>
+                <a href="/">Kasa</a>
+                <Navbar/>
+            </header>
 
-                <main>
+            <main>
 
-                </main>
+            </main>
 
-                
-                <Footer/>
-             
-            </body>
-            </html>
+            
+            <Footer/>
         </>
 
     )
