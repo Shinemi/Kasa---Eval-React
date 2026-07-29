1bc7761001cdbe3baa26e97db1effe9a95799ccd
Shinemi <simon.lucas.83136@gmail.com>
2026-07-29 12:26:32 +0200

    Refactor Carousel, Navbar, Footer, Details, and Home styles for improved layout and consistency; integrate global styles for better UI coherence

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
+import '../styles/components/carousel.scss'
+import '../styles/base/global.scss'
 
 type CarouselProps = {
   pictures: string[]
