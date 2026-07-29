5156865df9872afb70d5440b41fc98ebb61c2bef
Shinemi <simon.lucas.83136@gmail.com>
2026-07-29 09:42:36 +0200

    Refactor NotFound page layout and styles for improved organization and user experience

--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -2,15 +2,17 @@ import Navbar from "../components/Navbar"
 import Footer from "../components/footer"
 import '../styles/pages/notFound.scss'
 import '../styles/base/global.scss'
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
     return (
         <>
             <Navbar/>
 
-            <main>
+            <main className="error404">
                 <h1>Erreur 404 - Not found</h1>
-                <p>La page à laquelle vous tenter d'accéder n'existe pas ou n'a pas été trouvée</p>
+                <p id="descriptif404">La page à laquelle vous tenter d'accéder n'existe pas ou n'a pas été trouvée</p>
+                <Link to={'/'} id="returnHome">Retourner à l'accueil</Link>
             </main>
 
             <Footer/>
